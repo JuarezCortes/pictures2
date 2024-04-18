c8578da1fafd80b61efb46d990f74a17a54470be
JuarezCortes <madhzow@gmail.com>
2024-04-18 16:30:33 -0700

    hice cambios en Api.jsx y en ImageShow.jsx

--- src/api.js
+++ src/api.js
@@ -1,14 +1,17 @@
 import axios from 'axios' 
 
-const searchImage = async(term)=> {
-    const url = 'htpps://api.unsplash.com/search/?client_id='
+const searchImage = async(term) => {
+    const url = 'htpps://api.unsplash.com/search/?client_id=wrBeybcu-MZu7CAqbyiKJsQ3CUbSbZqzkGwJ6H9e7xY'
     const response = await axios.get(url,{
         params:{
-            query:'cars'
+            query: term
         }
     })
     
-    console.log(response)
+    console.log(term)
+    console.log(response.data.results)
+
+return response.data.results
 }
 
 export default searchImage
