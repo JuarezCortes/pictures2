9a61a7d2230bf351415813b5632c169e9f1f9e6f
JuarezCortes <madhzow@gmail.com>
2024-04-22 14:49:36 -0700

    Se crearon dos nuevos archivos css

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import { useState } from 'react'
 import SearchBar from './components/SearchBar'
-
-
+import ImageList from './components/ImageList'
+import searchImage from '/api'
 import './App.css'
+
 
 function App() {
   const [images, setImages] = useState([])
 
   const handleFormSubmit = async(term) => {
     console.log('usted esta buscando con:', term)
+  
+  console.log('coco')
+  console.log(result)
+
+  setImages(result)
+  
   }
 
   return(
     <div>
       <h1>App</h1>
       <SearchBar onSubmit={handleSubmit}/>
+      <ImageList images={images} />
     </div>
   )
 }
